a13462524fbba4ea821e720915061fd985620f5b
anism <muhamadanis10@gmail.com>
2025-04-30 11:04:29 +0700

    Add creating file from image uri function

--- app/src/main/java/com/example/retinopati/core/utils/CameraFileUtils.kt
+++ app/src/main/java/com/example/retinopati/core/utils/CameraFileUtils.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -44,6 +45,23 @@ suspend fun File.reduceFileImageAsync(): File = withContext(Dispatchers.IO) {
 
     bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
     return@withContext file
+}
+
+fun createCustomTempFile(context: Context): File {
+    val filesDir = context.externalCacheDir
+    return File.createTempFile(timeStamp, ".jpg", filesDir)
+}
+
+fun uriToFile(context: Context, uri: Uri, ): File {
+    val tempFile = createCustomTempFile(context)
+    val inputStream = context.contentResolver.openInputStream(uri) as InputStream
+    val outputStream = FileOutputStream(tempFile)
+    val buffer = ByteArray(1024)
+    var length: Int
+    while (inputStream.read(buffer).also { length = it } > 0) outputStream.write(buffer, 0, length)
+    outputStream.close()
+    inputStream.close()
+    return tempFile
 }
 
 fun Bitmap.getRotatedBitmap(file: File): Bitmap {
