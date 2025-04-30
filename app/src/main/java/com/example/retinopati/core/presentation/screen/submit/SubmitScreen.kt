07982dcb62484bb15f89382a112b058c1dc95c20
anism <muhamadanis10@gmail.com>
2025-04-30 11:10:02 +0700

    Implement Predict function, Loading Progress still not working

--- app/src/main/java/com/example/retinopati/core/presentation/screen/submit/SubmitScreen.kt
+++ app/src/main/java/com/example/retinopati/core/presentation/screen/submit/SubmitScreen.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,12 +14,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -28,22 +31,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.retinopati.R
+import com.example.retinopati.core.data.network.ApiService
+import com.example.retinopati.core.di.AppContainer
 import com.example.retinopati.core.presentation.components.CustomButton
 import com.example.retinopati.core.presentation.components.RetinopatiToolbar
 import com.example.retinopati.core.utils.getImageUri
+import com.example.retinopati.core.utils.reduceFileImageAsync
+import com.example.retinopati.core.utils.uriToFile
 import com.example.retinopati.ui.theme.AppTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun SubmitContent(
     onClickBack: () -> Unit,
-    onClickProceed: (uri: String) -> Unit,
-    modifier: Modifier = Modifier
+    onClickProceed: (uri: String, prediction: String) -> Unit,
+    modifier: Modifier = Modifier,
+    apiService: ApiService = AppContainer.apiService,
 ) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val viewModel = remember { SubmitViewModel(apiService) }
+
+    val isLoading by viewModel.isLoading
 
     var previewImageUri: Uri by rememberSaveable { mutableStateOf(Uri.EMPTY) }
     var cameraUri by remember { mutableStateOf(Uri.EMPTY) }
@@ -89,44 +104,59 @@ fun SubmitContent(
         },
         modifier = modifier
     ) { innerPadding ->
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Spacer(modifier = Modifier.weight(0.3f))
-            AsyncImage(
-                model =
-                    if (previewImageUri.path?.isNotEmpty() == true) previewImageUri else R.drawable.image,
-                contentDescription = "null",
-                modifier = Modifier
-                    .width(360.dp)
-                    .height(320.dp)
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(40.dp),
+            if (isLoading) CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(innerPadding)
             ) {
-                CustomButton(onClick = {
-                    galleryLauncher.launch(
-                        PickVisualMediaRequest(
-                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                Spacer(modifier = Modifier.weight(0.3f))
+                AsyncImage(
+                    model =
+                        if (previewImageUri.path?.isNotEmpty() == true) previewImageUri else R.drawable.image,
+                    contentDescription = "null",
+                    modifier = Modifier
+                        .width(360.dp)
+                        .height(320.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(40.dp),
+                ) {
+                    CustomButton(onClick = {
+                        galleryLauncher.launch(
+                            PickVisualMediaRequest(
+                                ActivityResultContracts.PickVisualMedia.ImageOnly
+                            )
                         )
-                    )
-                }, text = "Galeri")
+                    }, text = "Galeri")
+                    CustomButton(onClick = {
+                        if (cameraPermissionState.status.isGranted) {
+                            cameraUri = getImageUri(context)
+                            cameraLauncher.launch(cameraUri)
+                        } else {
+                            cameraPermissionState.launchPermissionRequest()
+                            hasRequestedPermission = true
+                        }
+                    }, text = "Kamera")
+                }
                 CustomButton(onClick = {
-                    if (cameraPermissionState.status.isGranted) {
-                        cameraUri = getImageUri(context)
-                        cameraLauncher.launch(cameraUri)
-                    } else {
-                        cameraPermissionState.launchPermissionRequest()
-                        hasRequestedPermission = true
+                    coroutineScope.launch {
+                        if (previewImageUri != Uri.EMPTY) {
+                            val file = withContext(Dispatchers.IO) {
+                                uriToFile(context, previewImageUri).reduceFileImageAsync()
+                            }
+
+                            val predict = viewModel.predict(file)
+                            onClickProceed(previewImageUri.toString(),predict.prediction)
+                        }
                     }
-                }, text = "Kamera")
+                }, text = "PROSES")
+                Spacer(modifier = Modifier.weight(0.3f))
             }
-            CustomButton(onClick = { onClickProceed(previewImageUri.toString()) }, text = "PROSES")
-            Spacer(modifier = Modifier.weight(0.3f))
         }
     }
 }
@@ -135,7 +165,8 @@ fun SubmitContent(
 @Composable
 fun SubmitContentPreview() {
     AppTheme {
-        SubmitContent(onClickBack = {}, onClickProceed = {})
+        SubmitContent(onClickBack = {}, onClickProceed = { _, _ ->
+        })
     }
 }
 
